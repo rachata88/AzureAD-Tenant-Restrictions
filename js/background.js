5f06b76f670cb01e4da0615f263a54798a48bf7f
Rachata <rachata88@gmail.com>
2019-10-09 20:16:20 +0700

    Initialize this project.

---
+++ js/background.js
@@ -0,0 +1,33 @@
+﻿function httpHeaderInsertion(e) {
+
+    e.requestHeaders.push({
+        name: "Restrict-Access-To-Tenants",
+        value: ""
+    });
+
+    e.requestHeaders.push({
+        name: "Restrict-Access-Context", 
+        value: ""
+    });
+
+    return {requestHeaders: e.requestHeaders};
+}
+
+function addEventListener(browserObject){
+    browserObject.webRequest.onBeforeSendHeaders.addListener(
+        httpHeaderInsertion,
+        {
+            urls: [
+            "*://login.microsoftonline.com/*",
+            "*://login.microsoft.com/*",
+            "*://login.windows.net/*"]
+        },
+        ["blocking", "requestHeaders"]
+    );
+}
+
+if (typeof browser !== "undefined") {
+    addEventListener(browser);
+} else if (typeof chrome !== "undefined") {
+    addEventListener(chrome);
+}
